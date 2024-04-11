fb0377404f3c3e95637cf57225d1a23120b703cb
Senku <tolu_48@hotmail.fr>
2024-04-11 19:53:12 +0200

    Fix completion

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -1,7 +1,5 @@
 return {
-	{
-		'hrsh7th/cmp-nvim-lsp'
-	},
+	{ 'hrsh7th/cmp-nvim-lsp' },
 	{
 		'L3MON4D3/LuaSnip',
 		dependencies = {
@@ -25,18 +23,20 @@ return {
 				documentation = cmp.config.window.bordered(),
 			},
 			mapping = cmp.mapping.preset.insert({
+				['<C-k>'] = cmp.mapping.select_prev_item(), -- previous suggestion
+				['<C-j>'] = cmp.mapping.select_next_item(), -- next suggestion
 				['<C-b>'] = cmp.mapping.scroll_docs(-4),
 				['<C-f>'] = cmp.mapping.scroll_docs(4),
-				['<C-Space>'] = cmp.mapping.complete(),
-				['<C-e>'] = cmp.mapping.abort(),
-				['<CR>'] = cmp.mapping.confirm({ select = true }),
+				['<C-Space>'] = cmp.mapping.complete(), -- show completion suggestions
+				['<C-e>'] = cmp.mapping.abort(), -- close completion window
+				['<CR>'] = cmp.mapping.confirm({ select = false }),
 			}),
 			sources = cmp.config.sources({
-				--{ name = 'nvim_lsp' },
+				{ name = 'nvim_lsp' },
 				{ name = 'luasnip' },
-		}, {
 				{ name = 'buffer' },
-			})
+				{ name = 'path' },
+		}),
 		})
 	end
 	}
